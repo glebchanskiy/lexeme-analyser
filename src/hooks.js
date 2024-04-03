6b8241674796c1bc72555ae81f51a715e7d0b0ea
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-04-04 02:28:49 +0300

    refactor: ui help kit | langs

--- src/hooks.js
+++ src/hooks.js
@@ -2,7 +2,7 @@ import { useState } from "preact/hooks"
 import { fetchApi, fetchApiMock } from "./utils"
 
 
-const parseGpt = (obj) => {
+const parse = (obj) => {
   const map = new Map();
 
   const processToken = (token) => {
@@ -31,47 +31,9 @@ const parseGpt = (obj) => {
   };
 
   processToken(obj);
-  
+
   return map;
 };
-
-
-
-const parse = (obj) => {
-  const map = new Map()
-
-  if (!obj["token_list"]) {
-    if (obj["form"]) {
-      let type
-      try {
-        type = obj["analysis_list"][0]["tag_info"]
-      } catch (err) {
-        type = "undefined"
-      }
-      map.set(obj["form"], [type])
-
-    }
-  } else {
-    for (const token of obj["token_list"]) {
-      const nestedTokens = parse(token)
-
-      for (const t of nestedTokens) {
-
-        const form = t[0];
-        const type = t[1]
-
-        const old = map.get(form)
-        if (old) {
-          old.push(type)
-          map.set(form, old)
-        } else {
-          map.set(form, old)
-        }
-      }
-    }
-  }
-  return map
-}
 
 export const useLexemAnalys = ({ mock = false }) => {
   const [response, setResponse] = useState([])
@@ -80,7 +42,7 @@ export const useLexemAnalys = ({ mock = false }) => {
 
   const analyse = (text) =>
     (mock ? fetchApiMock(text) : fetchApi(text)).then((data) => {
-      const lexems = parseGpt(data)
+      const lexems = parse(data)
 
       console.log("Row data:", data)
       console.log("Parsed:", lexems)
@@ -93,5 +55,64 @@ export const useLexemAnalys = ({ mock = false }) => {
     response,
     clear,
     analyse,
+  }
+}
+
+export const useLocalization = (initialLang) => {
+
+  const [lang, setLang] = useState(initialLang)
+  const localization = {
+    en: {
+      help: `
+It is a text analysis tool. Here you can:
+Enter Text: Enter or upload your text in the text field. You can also upload a TXT or RTF file (the maximum file size is 5 MB).
+Analyze text: Click on the "Analyze" button to analyze the text. The program will count the number of unique words and display them with details such as the type of word and the number of repetitions.
+Sort the results: You can sort the results alphabetically by clicking on the "Sort alphabetically" switch.
+Change the language: Click on the language switch to switch between English and Russian interface languages.
+Clear text: Click on the "Clear" button to clear the input field and the analysis results.
+Get help: If you need help, hover over the question icon in the upper-left corner of the page.
+`,
+      output: {
+        alphobetSorting: 'Alphabetical sorting',
+        placeholderTextaria: 'Write your text here...'
+      },
+      buttons: {
+        analyse: 'Analyse',
+        clear: 'Clear',
+        chooseFile: 'Click to choose file',
+        toggleLang: 'Toggle lang'
+      }
+    },
+    ru: {
+      help: `
+      Это инструмент для анализа текста. Здесь вы можете:
+      Вводить текст: Введите или загрузите свой текст в текстовое поле. Вы также можете загрузить файл формата TXT или RTF (максимальный размер файла - 5 МБ).
+      Анализировать текст: Нажмите на кнопку "Анализировать", чтобы проанализировать текст. Программа подсчитает количество уникальных слов и выведет их с подробностями, такими как тип слова и количество повторений.
+      Сортировать результаты: Вы можете отсортировать результаты алфавитно, нажав на переключатель "Сортировка по алфавиту".
+      Сменить язык: Нажмите на переключатель языка, чтобы переключиться между английским и русским языками интерфейса.
+      Очистить текст: Нажмите на кнопку "Очистить", чтобы очистить поле ввода и результаты анализа.
+      Получить помощь: Если вам нужна помощь, наведите курсор мыши на значок вопроса в левом верхнем углу страницы.
+        `,
+      output: {
+        alphobetSorting: 'Алфовитный порядок',
+        placeholderTextaria: 'Напишите свой текст здесь...'
+      },
+      buttons: {
+        analyse: 'Анализировать',
+        clear: 'Очистить',
+        chooseFile: 'Нажмите, чтобы выбрать файл',
+        toggleLang: 'Изменить язык'
+      }
+    }
+  }
+
+
+  console.log('useLoc: ')
+  console.log('lang: ', lang)
+
+  return {
+    localization: localization[lang],
+    changeLangTo: (lang) => setLang(lang),
+    lang,
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { render } from "preact"
 import { useRef, useState } from "preact/hooks"
-import { useLexemAnalys } from "./hooks"
+import { useLexemAnalys, useLocalization } from "./hooks"
 import { QuestionIcon } from "./icons"
 import "./styles/output.css"
 import "./styles/tooltip.css"
@@ -13,17 +13,20 @@ export function App() {
   const { analyse, clear, response } = useLexemAnalys({ mock: false })
   const [sorted, setSorted] = useState(false)
 
+  const { localization, lang, changeLangTo } = useLocalization('en')
+
   return (
     <>
-      <Helper />
+      <Helper localization={localization} />
+      <ToggleLang localization={localization} lang={lang} changeLangTo={changeLangTo} />
+
       <div class="flex mt-10 ">
         <div class={`w-1/2 mx-auto`}>
-          <DropBox analyse={analyse} clear={clear} />
+          <DropBox analyse={analyse} clear={clear} localization={localization} />
         </div>
         <div
-          class={`${
-            response.length > 0 ? "visible" : "invisible !w-0 !opacity-0"
-          } w-1/2 mx-5 h-full flex flex-col transition-all duration- opacity-100 200 gap-5`}
+          class={`${response.length > 0 ? "visible" : "invisible !w-0 !opacity-0"
+            } w-1/2 mx-5 h-full flex flex-col transition-all duration- opacity-100 200 gap-5`}
         >
           <div class="flex border border-gray-600 rounded-md flex-wrap gap-2 p-10 mx-2 ">
             {[...response]
@@ -42,7 +45,7 @@ export function App() {
               ))}
           </div>
           <div class="flex items-center border border-gray-600 rounded-md gap-2 mx-2 px-10 py-3">
-            <label htmlFor="sorted">Alphabetical sorting</label>
+            <label htmlFor="sorted">{localization.output.alphobetSorting}</label>
             <input
               type="checkbox"
               id="sorted"
@@ -56,10 +59,33 @@ export function App() {
   )
 }
 
-const Helper = () => {
+const ToggleLang = ({ localization, changeLangTo, lang }) => {
+
+  const kek = () => {
+
+    if (lang === 'ru') {
+      changeLangTo('en')
+    } else {
+      changeLangTo('ru')
+    }
+  }
+  return (
+    <div class='absolute top-[26px] left-20'>
+      <label class="inline-flex items-center cursor-pointer">
+        <input onClick={kek} type="checkbox" value={lang} class="sr-only peer" />
+        <div class="text-black relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        <span class="ms-3 text-sm font-medium text-white dark:text-gray-300">{localization.buttons.toggleLang}</span>
+      </label>
+    </div>
+
+  )
+}
+
+const Helper = ({localization}) => {
+  
   return (
     <div
-      data-tooltip="Help text for stupid user"
+      data-tooltip={localization.help}
       data-tooltip-position="right"
       class="absolute top-5 left-5"
     >
@@ -68,7 +94,7 @@ const Helper = () => {
   )
 }
 
-const DropBox = ({ analyse, clear }) => {
+const DropBox = ({ analyse, clear, localization }) => {
   const textareaRef = useRef(null)
   const fileRef = useRef(null)
 
@@ -102,13 +128,15 @@ const DropBox = ({ analyse, clear }) => {
           id="message"
           rows={4}
           class="block p-2.5 w-8/12 text-sm text-gray-300 bg-gray-700 rounded-lg border border-gray-600 focus:outline-none"
-          placeholder="Write your text here..."
+          placeholder={localization.output.placeholderTextaria}
         ></textarea>
       </div>
       <div class="flex flex-col items-center">
+        <label for="file" class="block p-2 w-8/12 text-sm  border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 focus:border-blue-600 focus:border-2">{localization.buttons.chooseFile}</label>
         <input
+          id='file'
           ref={fileRef}
-          class="block w-8/12 text-sm  border rounded-lg cursor-pointer text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400 focus:border-blue-600 focus:border-2"
+          style={{ visibility: 'hidden' }}
           type="file"
           onInput={showFile}
         />
@@ -121,13 +149,13 @@ const DropBox = ({ analyse, clear }) => {
           onClick={() => analyse(textareaRef.current.value)}
           class="mx-auto border px-2 text-gray-200 border-gray-200 hover:border-gray-200 hover:text-gray-400 rounded cursor-pointer"
         >
-          Analyse
+          {localization.buttons.analyse}
         </button>
         <button
           onClick={() => clear()}
           class="mx-auto border px-2 text-gray-200 border-gray-200 hover:border-gray-200 hover:text-gray-400 rounded cursor-pointer"
         >
-          Clear
+          {localization.buttons.clear}
         </button>
       </div>
     </div>
